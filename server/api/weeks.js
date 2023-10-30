4b683b5162ed5ccbfbc9cdf81c7982857b131940
Loulian Liu <3l.liu444@gmail.com>
2023-10-30 17:12:24 -0400

    successfully get weeks via user id

--- server/api/weeks.js
+++ server/api/weeks.js
@@ -1,8 +1,35 @@
 const router = require("express").Router();
+const { requireUser } = require("./utils");
 
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-router.get(`/`, async (req, res) => {
-  
-})
+// Get the weeks of a user
+router.get(`/:id`, async (req, res) => {
+  try {
+    const allWeeks = await prisma.week.findMany({
+      where: {
+        userId: Number(req.params.id),
+      },
+      include: {
+        days: {
+          include: {
+            exercises: {
+              include: {
+                sets: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    allWeeks
+      ? res.status(200).send(allWeeks)
+      : res.status(400).send({ error: true, message: `Error getting weeks` });
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+module.exports = router;
